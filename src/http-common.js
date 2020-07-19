2da07da2d916eb572b89b1380fd52fe4a76de61f
dommend <jani@penttinen.fi>
2020-07-19 22:17:33 +0300

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
 
-     baseURL: process.env.RREACT_APP_ADMIN_BASE_URL + "/api/",
+     baseURL: process.env.REACT_APP_ADMIN_BASE_URL + "/api/",
   
   headers: {
     "Content-type": "application/json"
